0a224e2ae9fe5045edba3f4b29678d4ac705e8f8
javier <josjaviergarcia@gmail.com>
2016-12-20 16:26:32 +0100

    no quiero escribir nada

--- main.js
+++ main.js
@@ -4,18 +4,38 @@ $(document).ready(function(e) {
     if (elemento == "") {
       console.log("El elemento no existe");
     } else {
-      $("li:last").after('<li class="list-group-item">' + elemento + '</li>');
+      $("li:last").after('<li class="list-group-item"><span class="pull-left">' + elemento + '</span><span class="badge pull-left">0</span><button type="button" class="btn btn-default pull-right glyphicon glyphicon-shopping-cart"></button></li>');
       $("#elemento").val('');
     }
     event.preventDefault();
   });
 
   $("ul").on('click', 'li', function() {
+    var badge = $(this).children('.badge');
+
+    var contador = parseInt(badge.html());
+    if(contador==0){
+      badge.html(contador+1);
+     
+    }else{
+      badge.html('0');
+    }
+
+
     $(this).toggleClass('active');
 
-    var badge = $(this).children('.badge');
-    var contador = parseInt(badge.html());
-    badge.html(contador + 1);
+   
 
+  });
+  $('ul').on('click', 'button',function(e){
+    
+       var badge = $(this).siblings('.badge');
+
+    var contador = parseInt(badge.html());
+    if(contador==0){$(this).parent().addClass('active');}
+    badge.html(contador + 1);
+     // event.preventDefault;
+
+     return false;//evitamos activar la función click de li y no nos cambiará la clase a active
   });
 });
